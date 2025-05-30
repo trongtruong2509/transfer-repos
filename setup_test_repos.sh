dda3f62cfaf8bae5142857dd16f75ace96ef535b
Trong Truong <e-trong.truong@makeinfinite.com>
2025-05-31 00:41:18 +0700

    new list

--- setup_test_repos.sh
+++ setup_test_repos.sh
@@ -305,7 +305,7 @@ create_repo() {
 }
 
 # Generate a timestamp suffix for uniqueness
-TIMESTAMP="20250531"
+TIMESTAMP="20250533"
 print_status "Using hardcoded timestamp suffix for unique repositories: $TIMESTAMP"
 
 # Create repositories in first organization
